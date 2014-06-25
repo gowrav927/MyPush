ea3b15a06f76204db889e2615218dfe05b6e6a87
gowrav927 <gowrav.divasigamani@gmail.com>
2014-06-25 15:01:53 -0500

    Update Jesus.java

--- src/com/Jesus.java
+++ src/com/Jesus.java
@@ -2,6 +2,6 @@ package com;
 
 public class Jesus {
 public static void main(String args[]){
-	System.out.println("Trust in Jesus christ & love him & mom & dad");
+	System.out.println("Trust in Jesus christ & love him & mom & dad the lord himself");
 }
 }
